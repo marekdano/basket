a458fbcd6ef186faa991113fa05acd5fb2db1aaf
Marek Dano <mk.dano@gmail.com>
2016-05-07 00:31:35 +0100

    Add API for items.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  resources :items, only: :index
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :items, only: :index, defaults: {format: :json}
+    end 
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
